f99ad6bc0998df8bdbba1a33b151993f442c8818
Zinedine <senaraeli@gmail.com>
2025-08-02 11:03:08 -0400

    Added Go import completion

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -6,6 +6,8 @@ return {
     filetypes = { "go", "gomod", "gowork", "gotmpl" },
     settings = {
         gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
             analyses = {
                 unusedparams = true,
             },
